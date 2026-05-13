32ff0e92101bf314d1747012f7b2ba87b34ac8b3
Aditya Soran <adityasoran@gmail.com>
2026-05-13 12:05:38 +0530

    Done

--- backend/main.py
+++ backend/main.py
@@ -22,8 +22,13 @@ os.environ["LANGCHAIN_PROJECT"] = "VibeAI"
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://chat-bot-langgraph.vercel.app"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://chat-bot-langgraph.vercel.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
